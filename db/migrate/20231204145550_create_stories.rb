86278ba8f35092467fa7a4ac10289be978c39218
kiran aziz <kiranaziz637@gmail.com>
2023-12-04 15:13:45 +0000

    added models and controllers

---
+++ db/migrate/20231204145550_create_stories.rb
@@ -0,0 +1,13 @@
+class CreateStories < ActiveRecord::Migration[7.1]
+  def change
+    create_table :stories do |t|
+      t.string :title
+      t.string :genre
+      t.text :content
+      t.text :synopsis
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

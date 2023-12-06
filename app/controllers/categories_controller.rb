d8e0e599254dcbfe254ad2a9b825ffeaf07d0c43
Romance Gemayel <25gemayelromance@gmail.com>
2023-12-06 15:20:03 +0200

    added show page for categories and updated seed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,9 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def show
+    @category = Category.find(params[:id])
+    @stories = Story.select { |story| story.category_id == @category.id}
+  end
 end
